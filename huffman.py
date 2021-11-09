571ff302a63300bfe603c7374359680c621be39b
Philipp Jost <philipp.jost@protonmail.com>
2021-11-09 14:40:02 +0900

    feat: DCT for sub matrices / 8x8 units

--- huffman.py
+++ huffman.py
@@ -1,4 +1,7 @@
 from PIL import Image
+from matplotlib import pyplot as plt
+import numpy as np
+from dct import DCT, Q
 
 im = Image.open("./lenargb.jpg")
 print(im.format, im.size, im.mode)
@@ -10,6 +13,34 @@ print(ycbcr.format, ycbcr.size, ycbcr.mode)
 # split the image into individual bands
 (R, G, B) = im.split()
 (Y, Cb, Cr) = ycbcr.split()
+
+# translate indexing from (512,512) -> (64, 64)@(8,8)
+#┌───────64x64...
+#│┌┐┌┐─8x8
+#│└┘└┘─[1,0]
+#│┌┐─[0,1]
+#.└┘
+def index64(M,x,y):
+    return M[(x*8):(x*8)+8,(y*8):(y*8)+8]
+
+Ya = np.matrix(Y, dtype=float)
+Ya -= 128
+print(index64(Ya,0,0))
+
+Yc = DCT(index64(Ya,0,0))
+Yc = np.around(Yc/Q)
+
+print(Yc)
+
+
+## get all DC components
+#for x in range(64) y in range(y):
+#
+#    # get coefficients
+#    Yc = DCT(index64(Ya,0,0))
+#
+#    # quantize
+#    Yc = np.around(Yc/Q)
 
 # entropy encode the quantized coefficients
 
